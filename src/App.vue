dd468c3c403585207d7370f6c21987804c357abf
divasilevski <divasilevski@gmail.com>
2020-01-02 22:01:00 +0300

    Created router-buttons

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/user">User</router-link>
-    </div>
-    <router-view />
+  <div class="app">
+    <RouterButton></RouterButton>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// @ is an alias to /src
+import RouterButton from "@/components/router-button.vue";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "app",
+  components: {
+    RouterButton
+  }
+};
+</script>
---
+++ src/components/router-button.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="navbar-item">
+    <div class="buttons">
+      <b-button tag="router-link" to="/" type="is-success">Home</b-button>
+      <b-button tag="router-link" to="/user" type="is-success">User</b-button>
+    </div>
+  </div>
+</template>
