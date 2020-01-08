74f09a37c80dd33323e0e466b48b04df874629b0
divasilevski <divasilevski@gmail.com>
2020-01-08 17:36:51 +0300

    sign-up rebuilt

--- src/components/authentication/sign-up.vue
+++ src/components/authentication/sign-up.vue
@@ -6,34 +6,92 @@
           <!-- 1/3 Horizontal center -->
 
           <section>
+            <h1 class="title is-1 has-text-centered">Create your account</h1>
+
+            <!-- USERNAME -->
+            <b-field>
+              <p class="control has-icons-left">
+                <b-input placeholder="Username" v-model="user_name"></b-input>
+                <span class="icon is-small is-left">
+                  <i class="fa fa-user" aria-hidden="true"></i>
+                </span>
+              </p>
+            </b-field>
+
+            <!-- EMAIL ADRESS -->
+            <b-field>
+              <p class="control has-icons-left">
+                <b-input
+                  placeholder="Email address"
+                  type="email"
+                  v-model="user_mail"
+                ></b-input>
+                <span class="icon is-small is-left">
+                  <i class="fas fa-envelope"></i>
+                </span>
+              </p>
+            </b-field>
+
+            <!-- PASSWORD -->
+            <b-field>
+              <p class="control has-icons-left">
+                <b-input
+                  type="password"
+                  placeholder="Create password"
+                  v-model="user_password"
+                  password-reveal
+                ></b-input>
+                <span class="icon is-small is-left">
+                  <i class="fas fa-lock"></i>
+                </span>
+              </p>
+            </b-field>
+
+            <!-- CONFIRM PASSWORD -->
+            <b-field>
+              <p class="control has-icons-left">
+                <b-input
+                  type="password"
+                  placeholder="Confirm password"
+                  v-model="confirm_password"
+                  password-reveal
+                ></b-input>
+                <span class="icon is-small is-left">
+                  <i class="fa fa-key" aria-hidden="true"></i>
+                </span>
+              </p>
+            </b-field>
+
+            <!-- REGGISTRATE BUTTON -->
             <div class="field">
-              <b-input placeholder="UserName" v-model="user_name"></b-input>
-            </div>
-            <div class="field">
-              <b-input placeholder="Email" type="email" v-model="user_mail"></b-input>
-            </div>
-            <div class="field">
-              <b-input
-                placeholder="Password"
-                type="password"
-                v-model="user_password"
-                password-reveal
-              ></b-input>
+              <b-button
+                type="is-dark"
+                @click="registrate"
+                :loading="loading"
+                expanded
+              >
+                Sign Up
+              </b-button>
             </div>
 
-            <!-- Registrate button -->
-            <div class="field">
-              <b-button @click="registrate" :loading="loading" expanded>SignUp</b-button>
+            <!-- --------------------------SEPARATOR-------------------------- -->
+            <div class="field has-text-centered">
+              <span style="color: #D6D6D6">———————— or ————————</span>
             </div>
 
-            <!-- Tag with error -->
-            <b-tag
-              v-if="isTag"
-              attached
-              closable
-              aria-close-label="Close tag"
-              @close="isTag = false"
-            >{{ errorTag }}</b-tag>
+            <!-- GOOGLE -->
+            <div class="field">
+              <b-button icon-left="fab fa-google" icon-pack="fab" expanded>
+                Sign In using Google
+              </b-button>
+            </div>
+
+            <!-- GITHUB -->
+            <div class="field">
+              <b-button icon-left="fab fa-github" icon-pack="fab" expanded>
+                Sign In using GitHub
+              </b-button>
+            </div>
           </section>
 
           <!-- 1/3 Horizontal center -->
@@ -66,8 +124,12 @@ export default {
         .then(() => {
           this.$router.push("/user");
         })
-        .catch(() => {
-          this.isTag = true;
+        .catch(error => {
+          this.$buefy.toast.open({
+            message: error,
+            position: "is-bottom",
+            type: "is-dark"
+          });
         });
     }
   },
